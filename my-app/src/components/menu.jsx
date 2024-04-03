66305181dfab145a4d2f17cadf0203f80a6d4ee9
666Deadsoul <sarinamaharjan234@gmail.com>
2024-04-03 19:43:39 +0545

    menu

---
+++ my-app/src/components/menu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './menu.css';
+
+const Menu=()=>{
+    return(
+        <div id="navbar">
+        <a href="#welcome-section">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </div>
+    );
+}
+export default Menu;
