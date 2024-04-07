ebce5792d7de12156c9d55ab382a2212c33fa718
666Deadsoul <sarinamaharjan234@gmail.com>
2024-04-07 19:31:04 +0545

    Contact_css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import Menu from './components/menu';
 import Home from './components/home';
 import About from './components/about';
 import Project from './components/project';
+import Contact from './components/contact';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
     <Home/>
     <About/>
     <Project/>
+    <Contact/>
     </div>
   );
 }
