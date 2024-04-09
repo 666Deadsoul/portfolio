7075e4c6b463fd0e2b6f80c229de3ed3a7310ab3
666Deadsoul <sarinamaharjan234@gmail.com>
2024-04-09 18:46:12 +0545

    Tech_stack

--- my-app/src/components/home.jsx
+++ my-app/src/components/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './home.css';
 import './style.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import {faGithub, faLinkedin, faHtml5, faCss3, faJs, faReact, faFigma } from '@fortawesome/free-brands-svg-icons';
 import Profile from '../images/profile.jpg';
 
 const Home=()=>{
@@ -11,11 +11,23 @@ const Home=()=>{
          <div id="welcome-section">
           <div id="me_section">
               <span id="ani_text">I am a Front-End Developer.</span>
-              <p>Hello! I am Sarina Maharjan. A student and a passionate Frontend Developer.😁</p>
+              <p>Hello! I am Sarina Maharjan. A student and a passionate Frontend Developer from Kathmandu.😁</p>
           </div>
           <div className="Smedia">
             <a href="https://github.com/666Deadsoul"><FontAwesomeIcon icon={faGithub} /></a>
             <a href="https://www.linkedin.com/in/sarina-maharjan-b29609244/"><FontAwesomeIcon icon={faLinkedin} /></a>
+          </div>
+          <div id="tech_stack">
+              <div className="ts">
+                <h2>Tech Stack</h2>
+              </div>
+              <div className="ts_ic">
+                <FontAwesomeIcon icon={faHtml5} />
+                <FontAwesomeIcon icon={faCss3} />
+                <FontAwesomeIcon icon={faJs} />
+                <FontAwesomeIcon icon={faReact} />
+                <FontAwesomeIcon icon={faFigma} />
+              </div>
           </div>
         </div>
         <div id="Profile">
