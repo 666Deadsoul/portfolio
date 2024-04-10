dc4a0c97b0895eef60f0013923fc17c4971fa856
666Deadsoul <sarinamaharjan234@gmail.com>
2024-04-10 17:26:34 +0545

    menu

--- my-app/src/components/menu.jsx
+++ my-app/src/components/menu.jsx
@@ -4,10 +4,16 @@ import './menu.css';
 const Menu=()=>{
     return(
         <div id="navbar">
-        <a href="#welcome-section">Home</a>
-        <a href="#about">About</a>
-        <a href="#projects">Projects</a>
-        <a href="#contact">Contact</a>
+        <div>
+          <p id="logo">Sarina.io</p>
+        </div>
+        <div>
+          <a href="#welcome-section">Home</a>
+          <a href="#about">About</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
+        </div>
+        
       </div>
     );
 }
