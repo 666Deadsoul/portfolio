908d6ea722b7724fc34f55f18ee9178ccb23c0a2
666Deadsoul <sarinamaharjan234@gmail.com>
2024-04-15 21:17:24 +0545

    project layout

--- my-app/src/components/project.jsx
+++ my-app/src/components/project.jsx
@@ -25,10 +25,13 @@ const Project=()=>{
               <img src="https://preview.redd.it/i-got-bored-so-i-decided-to-draw-a-random-image-on-the-v0-4ig97vv85vjb1.png?width=640&crop=smart&auto=webp&s=22ed6cc79cba3013b84967f32726d087e539b699"/>
               </div>
               <div className="proj_title">
-                <h3>Draw</h3>
+                <h3>Speedy</h3>
               </div>
               <div className="proj_desc">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsum aliquid placeat doloribus itaque vero dolore asperiores assumenda!</p>
+              </div>
+              <div className="proj_tech">
+                <h3>Tech:  <span>HTML, CSS, JS, React</span></h3>
               </div>
             </div>
             <div className="project_card">
@@ -36,10 +39,13 @@ const Project=()=>{
               <img src="https://preview.redd.it/i-got-bored-so-i-decided-to-draw-a-random-image-on-the-v0-4ig97vv85vjb1.png?width=640&crop=smart&auto=webp&s=22ed6cc79cba3013b84967f32726d087e539b699"/>
               </div>
               <div className="proj_title">
-                <h3>Recipe</h3>
+                <h3>Speedy</h3>
               </div>
               <div className="proj_desc">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsum aliquid placeat doloribus itaque vero dolore asperiores assumenda!</p>
+              </div>
+              <div className="proj_tech">
+                <h3>Tech:  <span>HTML, CSS, JS, React</span></h3>
               </div>
             </div>
           </div>
