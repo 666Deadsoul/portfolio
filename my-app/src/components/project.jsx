5511669f7886714c1dd69e0101877981de519b33
666Deadsoul <sarinamaharjan234@gmail.com>
2024-04-15 20:29:03 +0545

    cards css

--- my-app/src/components/project.jsx
+++ my-app/src/components/project.jsx
@@ -17,6 +17,28 @@ const Project=()=>{
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsum aliquid placeat doloribus itaque vero dolore asperiores assumenda!</p>
               </div>
             </div>
+            <div className="project_card">
+              <div className="proj_img">
+              <img src="https://preview.redd.it/i-got-bored-so-i-decided-to-draw-a-random-image-on-the-v0-4ig97vv85vjb1.png?width=640&crop=smart&auto=webp&s=22ed6cc79cba3013b84967f32726d087e539b699"/>
+              </div>
+              <div className="proj_title">
+                <h3>Title1</h3>
+              </div>
+              <div className="proj_desc">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsum aliquid placeat doloribus itaque vero dolore asperiores assumenda!</p>
+              </div>
+            </div>
+            <div className="project_card">
+              <div className="proj_img">
+              <img src="https://preview.redd.it/i-got-bored-so-i-decided-to-draw-a-random-image-on-the-v0-4ig97vv85vjb1.png?width=640&crop=smart&auto=webp&s=22ed6cc79cba3013b84967f32726d087e539b699"/>
+              </div>
+              <div className="proj_title">
+                <h3>Title1</h3>
+              </div>
+              <div className="proj_desc">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsum aliquid placeat doloribus itaque vero dolore asperiores assumenda!</p>
+              </div>
+            </div>
           </div>
       </div>
     );
