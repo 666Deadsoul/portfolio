2ea61f71d955f9d12f3d6247e3bcea9a57ab9edd
666Deadsoul <sarinamaharjan234@gmail.com>
2024-04-17 20:46:06 +0545

    about respo

--- my-app/src/components/about.jsx
+++ my-app/src/components/about.jsx
@@ -6,18 +6,18 @@ const About=()=>{
     return(
         <div id="main_ab">
             <div id="about">
-        <div className="av">
-            <img src={Av} id="S_av"/>
-        </div>
-            <div id="detail">
-            <h1>About Me</h1>
-            <p>
-            Hi, I am Sarina Maharjan, currently a student and my specific interest lies in Frontend Development as well as UI/UX Designing.
-            I am a proactive person with a genuine eagerness to learn and expand my knowledge. 
-            </p>
-            <button>Download Resume</button>
+                <div className="av">
+                    <img src={Av} id="S_av"/>
+                </div>
+                <div id="detail">
+                    <h1>About Me</h1>
+                    <p>
+                    Hi, I am Sarina Maharjan, currently a student and my specific interest lies in Frontend Development as well as UI/UX Designing.
+                    I am a proactive person with a genuine eagerness to learn and expand my knowledge. 
+                    </p>
+                    <button>Download Resume</button>
+                </div>
             </div>
-      </div>
         </div>
         
     );
