179702cd8b419040d37d335f8fa6654bf3c0cbea
666Deadsoul <sarinamaharjan234@gmail.com>
2024-04-17 20:03:59 +0545

    contact made

--- my-app/src/components/contact.jsx
+++ my-app/src/components/contact.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import './contact.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
 
 const Contact =()=>{
     return(
         <div id="contact">
-        <h1>Contact Me</h1>
-        <a href="#" id="profile-link" target="_blank">LinkedIn</a>
-        <p>Or</p>
-        <form id="cont_f">
-        <div>
-            <textarea name="comments" id="comments" placeholder="Leave a Message">
-            </textarea>
-        </div>
-        <input type="submit" value="Send" id="s_btn"/>
-        </form>
+            <h1>Contact Me 👇</h1>
+            <div id="cLinks">
+                <div className="li">
+                    <a href="https://www.linkedin.com/in/sarina-maharjan-b29609244/" className="links"><FontAwesomeIcon icon={faLinkedin} /></a>
+                </div>
+                <div className="emailme">
+                    <a href="#" className="links"><FontAwesomeIcon icon={faEnvelope}/></a>
+                </div>
+            </div>
+           
         </div>
     );
 }
-export default Contact;
+export default Contact; 
