0f7f5678126eea35d815118e3d197f31d02f5d91
666Deadsoul <sarinamaharjan234@gmail.com>
2024-04-18 20:56:56 +0545

    hamburger menu

--- my-app/src/components/menu.jsx
+++ my-app/src/components/menu.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import './menu.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
 
 const Menu=()=>{
     return(
         <div id="navbar">
+        <input type="checkbox" id="checkbtn"/>
+            <label htmlFor="checkbtn" className="check-logo">
+            <FontAwesomeIcon icon={faBars} />
+            </label>
         <div>
           <p id="logo">Sarina.io</p>
         </div>
-        <div>
+        <div id="m_items">
           <a href="#welcome-section">Home</a>
           <a href="#about">About</a>
           <a href="#projects">Projects</a>
